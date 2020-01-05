40bdaaa247ab78153645ea57b14f6884c7962e29
Shunya <yamasyunizig1216@gmail.com>
2020-01-06 01:44:30 +0900

    Profile design作成

--- src/components/user/Profile.vue
+++ src/components/user/Profile.vue
@@ -1,5 +1,61 @@
 <template>
    <v-container>
-      <h1>test</h1>
+      <v-row align="center" justify="center">
+         <v-col col="12" md="4">
+            <br><br>
+            <figure class="avatar">
+                  <img src="https://placehold.it/128x128">
+            </figure>
+         </v-col>
+         <v-col col="12" md="4" lg="3">
+            <v-card-text >
+               <v-card-title>Yourname</v-card-title>
+               this is a place for description.
+            </v-card-text>
+         </v-col>
+      </v-row>
+      <!-- <br><br> -->
+
+      <v-row>
+         <v-col cols="12" lg="4" xs="12">
+            <v-card>
+               <v-card-title>test</v-card-title>
+               
+            </v-card>
+         </v-col>
+
+         <v-col cols="12" lg="4" xs="12">
+            <v-card>
+               <v-card-title>test</v-card-title>
+               
+            </v-card>
+         </v-col>
+
+         <v-col cols="12" lg="4" xs="12">
+            <v-card>
+               <v-card-title>test</v-card-title>
+               
+            </v-card>
+         </v-col>
+      </v-row>
    </v-container>
 </template>
+
+<script>
+export default {
+   
+}
+</script>
+
+<style scoped>
+  .avatar {
+    text-align: center;
+  }
+  .avatar img {
+    padding: 5px;
+    background: #fff;
+    border-radius: 50%;
+    -webkit-box-shadow: 0 2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1);
+    box-shadow: 0 2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1);
+  }
+</style>
